660994cf0c2f50bbabce4d485e45442bf297ecbf
patsadow2 <patsadow2@gmail.com>
2016-06-29 22:11:44 +0200

    v 0.0.1

---
+++ src/ApiAuctionShop/Models/MobileAuctionSender.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ApiAuctionShop.Models
+{
+    public class MobileAuctionSender
+    {
+        public int ID { get; set; }
+        public int duration { get; set; }
+
+        public byte[] ImageData { get; set; }
+        public string ImageMimeType { get; set; }
+
+        public string description { get; set; }
+
+        public int price { get; set; }
+
+        public string title { get; set; }
+
+    }
+}
